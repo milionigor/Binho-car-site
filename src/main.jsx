2796b8d9506bd636e084b796ec8eebe858ef0ce8
milionigor <igorsouzayt21@gmail.com>
2026-03-28 17:19:06 -0300

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import DetalhesCarro from "./pages/DetalhesCarro"; // 1. Importamos a nova página
+import DetalhesCarro from "./pages/DetalhesCarro";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* 🚀 O segredo para o GitHub Pages não ficar branco é o basename abaixo: */}
+    <BrowserRouter basename="/Binho-car-site">
       <Routes>
         {/* Rota da Página Inicial */}
         <Route path="/" element={<App />} />
 
-        {/* 2. Ativamos a rota de detalhes removendo os comentários */}
+        {/* Rota de Detalhes do Carro */}
         <Route path="/carro/:id" element={<DetalhesCarro />} />
       </Routes>
     </BrowserRouter>
