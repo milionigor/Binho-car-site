5b09cb81de36ca284e868f839c744598c0ec6465
milionigor <igorsouzayt21@gmail.com>
2026-05-19 19:13:59 -0300

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Trocamos para HashRouter
 import App from "./App.jsx";
 import DetalhesCarro from "./pages/DetalhesCarro";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* 🚀 O segredo para o GitHub Pages não ficar branco é o basename abaixo: */}
-    <BrowserRouter basename="/Binho-car-site">
+    {/* Com HashRouter, não precisamos mais do basename! */}
+    <HashRouter>
       <Routes>
-        {/* Rota da Página Inicial */}
         <Route path="/" element={<App />} />
-
-        {/* Rota de Detalhes do Carro */}
         <Route path="/carro/:id" element={<DetalhesCarro />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
